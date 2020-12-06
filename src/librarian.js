d5ac1ec107ee2cbf984d45a96b89366110c104be
Anneke Miers <annekemiers.am@gmail.com>
2020-12-06 12:51:19 -0700

    Attempt at finishing iteration

--- src/librarian.js
+++ src/librarian.js
@@ -16,18 +16,18 @@ greetPatron(patron, boolean) {
 }
 
 findBook(book) {
-  const allBooks = [...library.shelves.fantasy, ...library.shelves.fiction, ...library.shelves.nonFiction ]
-
-  console.log('allBooks', allBooks)
-
   for (var i = 0; i < library.shelves.fantasy.length; i++) {
     if (library.shelves.fantasy[i].title === book) {
-      librarian.library.checkoutBook();
+      this.library.checkoutBook();
       return `Yes, we have ${book}`;
     } else {
       return `Sorry, we do not have ${book}`
     }
     }
+  }
+
+  calculateLateFee(days) {
+    return Math.round(days * .25);
   }
 }
 module.exports = Librarian;
